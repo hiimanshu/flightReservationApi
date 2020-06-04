5a4d987918a3d20aa11f0a4c6ea5ca2aeecf27d2
Himanshu Sharma <sharmahimanshu2798@gmail.com>
2020-06-04 17:25:22 +0530

    Created Project

---
+++ flightReservationApi/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for flightReservationApi project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flightReservationApi.settings')
+
+application = get_asgi_application()
